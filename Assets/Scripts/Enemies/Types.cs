42011dd9a843ee8e97548e57144ab07389afa197
wert <ilya.f.s01@mail.ru>
2017-11-01 17:41:33 +0800

    Пофиксил дерьмо

--- Assets/Scripts/Enemies/Types.cs
+++ Assets/Scripts/Enemies/Types.cs
@@ -4,25 +4,14 @@ using UnityEngine;
 
 namespace Types
 {
-<<<<<<< HEAD
     public enum TypeOfItem
     {
         HEAD,
         CHEST,
         FEET,
         WEAPON
-=======
-    public enum TypeOfItem 
-	{
-        WEAPON,
-		HELM,
-		BODY_ARM,
-		SHIELD,
-		BOOTS
->>>>>>> 26fb938a9a46c1d91bece8f34b7c2ec2aec26221
+
     }
-
-
 }
 ///Бревно
 /// Там жук
